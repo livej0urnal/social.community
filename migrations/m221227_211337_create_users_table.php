1885eefa1b05412eb2e6a564010bc5fe25e0d488
Igor Filippov <anubis3009@gmail.com>
2022-12-27 23:17:19 +0200

    #rebase-template

--- migrations/m221227_211337_create_users_table.php
+++ migrations/m221227_211337_create_users_table.php
@@ -14,6 +14,8 @@ class m221227_211337_create_users_table extends Migration
     {
         $this->createTable('{{%users}}', [
             'id' => $this->primaryKey(),
+            'email' => $this->string(255)->notNull(),
+            'password' => $this->string(255)->notNull()
         ]);
     }
 
