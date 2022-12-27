ffb39bbcd6f85e23fb00bfe4696846957b7a1e7c
Igor Filippov <anubis3009@gmail.com>
2022-12-27 22:12:17 +0200

    #rebase-template

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,7 +14,7 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?>" data-theme="dark">
 <head>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->registerCsrfMetaTags() ?>
@@ -28,6 +28,15 @@ AppAsset::register($this);
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GMKQ4P9YMZ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-GMKQ4P9YMZ');
+    </script>
     <?php $this->head() ?>
 </head>
 <body>
