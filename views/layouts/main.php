47e1b1fa8c5ec4234198756780776d580e908a0a
Igor Filippov <anubis3009@gmail.com>
2022-12-27 22:05:12 +0200

    #rebase-template

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,12 +3,12 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use app\assets\AppAsset;
 use app\widgets\Alert;
-use yii\bootstrap4\Breadcrumbs;
-use yii\bootstrap4\Html;
-use yii\bootstrap4\Nav;
-use yii\bootstrap4\NavBar;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
