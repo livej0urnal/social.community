061b7079416f641f219b9d5e1ed1a1a0e40c12d0
Igor Filippov <anubis3009@gmail.com>
2022-12-28 16:05:39 +0200

    #signup-model

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -14,6 +14,7 @@ class AccountController extends AppController
         $model = new Signup();
         $model->attributes = Yii::$app->request->post('Signup');
         if($model->validate() && $model->signup()) {
+            $model = new Signup();
             Yii::$app->session->setFlash('success', 'Signup success!');
             return $this->redirect('account/login');
 
