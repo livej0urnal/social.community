80c1ea3a1c9ce915488e21b2d377b4f4d8850b68
Igor Filippov <anubis3009@gmail.com>
2022-12-28 16:04:46 +0200

    #signup-model

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -351,14 +351,6 @@ Header END -->
             </div>
         <?php endif; ?>
 
-        <?php if(Yii::$app->session->hasFlash('error')) : ?>
-            <div class="alert alert-success alert-dismissable" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <?php echo Yii::$app->session->getFlash('error'); ?>
-            </div>
-        <?php endif; ?>
         <?= $content ?>
     </div>
     <!-- Container END -->
