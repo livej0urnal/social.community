952e6ab68add23b2ff2228bb2354ea42db240ab6
Igor Filippov <anubis3009@gmail.com>
2022-12-28 16:58:45 +0200

    #login-form

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -8,28 +8,6 @@ use yii\filters\AccessControl;
 class AppController extends Controller
 {
 
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['login', 'logout'], // действия в контроллере
-                'rules' => [ // правила к действиям
-                    [
-                        'allow' => true,
-                        'actions' => ['login'], // действия в контроллере
-                        'roles' => ['?'], // Доступ к действиям только для не авторизованных пользователей
-                    ],
-                    [
-                        'allow' => true,
-                        'actions' => ['logout'], // действия в контроллере
-                        'roles' => ['@'], // Доступ к действиям только для авторизованных пользователей
-                    ],
-                ],
-            ],
-        ];
-    }
-
     protected function setMeta($title = null, $keywords = null, $description = null)
     {
         $this->view->title = $title . ' - Best Social App';
