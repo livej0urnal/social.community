8896d5dce520a661aa80a26d08011a7eb6c7e5d0
Igor Filippov <anubis3009@gmail.com>
2022-12-28 15:21:53 +0200

    #rebase-template

---
+++ models/Users.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $password
+ */
+class Users extends ActiveRecord implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'password'], 'required'],
+            [['email', 'password'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'password' => 'Password',
+        ];
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = sha1($password);
+    }
+
+    public function validatePassword($password)
+    {
+        return $this->password === sha1($password);
+    }
+
+    public static function findIdentity($id)
+    {
+        // TODO: Implement findIdentity() method.
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // TODO: Implement findIdentityByAccessToken() method.
+        // return static::findOne(['access_token' => $token]);
+    }
+
+    public function getId()
+    {
+        // TODO: Implement getId() method.
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        // TODO: Implement getAuthKey() method.
+//        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        // TODO: Implement validateAuthKey() method.
+        //return $this->authKey === $auth_key;
+    }
+}
