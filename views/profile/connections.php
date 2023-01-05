9d535d2732c66c5ca65fc3a8a685bc5ccd719699
Igor Filippov <anubis3009@gmail.com>
2023-01-05 16:12:06 +0200

    #friends_and_feed_crud

--- views/profile/connections.php
+++ views/profile/connections.php
@@ -66,13 +66,11 @@
                     </div>
                 </div>
                 <?php endforeach; ?>
-                <ul class="pagination">
-                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                </ul>
+                <?php echo LinkPager::widget([
+                    'pagination' => $pages,
+                    'options' => ['class' => 'pagination tab-paginations'],
+                    'linkOptions' => ['class' => 'page-link'],
+                ]); ?>
             </div>
             <!-- Card body END -->
             <?php endif; ?>
