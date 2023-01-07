b8b99f92749bede46e25e7b11257d678f557e00b
Igor Filippov <anubis3009@gmail.com>
2023-01-07 19:49:39 +0200

    #delete-comments_with-post

--- models/PostForm.php
+++ models/PostForm.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use yii\base\Model;
+use yii\helpers\FileHelper;
+use yii\web\UploadedFile;
 
 class PostForm extends Model
 {
@@ -18,6 +20,19 @@ class PostForm extends Model
         ];
     }
 
+    public function upload()
+    {
+        if ($this->validate()) {
+            $path  = 'uploads/'. date('Y-m-d') ;
+            FileHelper::createDirectory($path);
+            $this->imageFile->saveAs($path . '/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+            $this->image = '/' . $path . '/' . $this->imageFile->baseName . '.' . $this->imageFile->extension;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function attributeLabels()
     {
         return [
@@ -25,4 +40,6 @@ class PostForm extends Model
             'content' => 'Content'
         ];
     }
+
+
 }
