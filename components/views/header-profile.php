6b08fd619f79860190c2cf8e41f5f603c80ec350
Igor Filippov <anubis3009@gmail.com>
2023-01-07 16:16:23 +0200

    #about-widget

--- components/views/header-profile.php
+++ components/views/header-profile.php
@@ -36,7 +36,7 @@
         <div class="card-footer mt-3 pt-2 pb-0">
             <!-- Nav profile pages -->
             <ul class="nav nav-bottom-line align-items-center justify-content-center justify-content-md-start mb-0 border-0" id="user-link-page">
-                <li class="nav-item"> <a class="nav-link link-profile-href" href="<?= Url::to(['page/profile' , 'id' => $page->id]) ?>"> Posts </a> </li>
+                <li class="nav-item"> <a class="nav-link link-profile-href" href="<?= Url::to(['page/profile' , 'id' => $page->id]) ?>"> Posts <span class="badge bg-success bg-opacity-10 text-success small"> <?php echo count($page->posts);?></span></a> </li>
                 <li class="nav-item"> <a class="nav-link link-profile-href" href="my-profile-about.html"> About </a> </li>
                 <li class="nav-item"> <a class="nav-link link-profile-href" href="<?= Url::to(['profile/connections' , 'user' => $page->user_id]) ?>"> Connections <span class="badge bg-success bg-opacity-10 text-success small"> <?php echo count($page->friends);?></span> </a> </li>
                 <li class="nav-item"> <a class="nav-link link-profile-href" href="my-profile-media.html"> Media</a> </li>
--- views/page/profile.php
+++ views/page/profile.php
@@ -52,8 +52,9 @@
                 <!-- Share feed toolbar END -->
             </div>
             <!-- Share feed END -->
-
+            <?php if(!empty($posts)) : ?>
             <!-- Card feed item START -->
+            <?php foreach ($posts as $post) : ?>
             <div class="card">
                 <!-- Card header START -->
                 <div class="card-header border-0 pb-0">
@@ -61,7 +62,9 @@
                         <div class="d-flex align-items-center">
                             <!-- Avatar -->
                             <div class="avatar avatar-story me-2">
-                                <a href="#!"> <img class="avatar-img rounded-circle" src="/images/avatar/04.jpg" alt=""> </a>
+                                <a href="<?= Url::to(['page/profile' , 'id' => $post->page_id]) ?>">
+                                    <?= Html::img($post->page->image, ['alt' => $post->page->display_name, 'class' => 'avatar-img rounded-circle']) ?>
+                                </a>
                             </div>
                             <!-- Info -->
                             <div>
@@ -289,96 +292,9 @@
                 </div>
                 <!-- Card footer END -->
             </div>
+            <?php endforeach; ?>
             <!-- Card feed item END -->
-
-            <!-- Card feed item START -->
-            <div class="card">
-
-                <div class="border-bottom">
-                    <p class="small mb-0 px-4 py-2"><i class="bi bi-heart-fill text-danger pe-1"></i>Sam Lanson likes this post</p>
-                </div>
-                <!-- Card header START -->
-                <div class="card-header border-0 pb-0">
-                    <div class="d-flex align-items-center justify-content-between">
-                        <div class="d-flex align-items-center">
-                            <!-- Avatar -->
-                            <div class="avatar me-2">
-                                <a href="#"> <img class="avatar-img rounded-circle" src="/images/logo/13.svg" alt=""> </a>
-                            </div>
-                            <!-- Title -->
-                            <div>
-                                <h6 class="card-title mb-0"> <a href="#!"> Apple Education </a></h6>
-                                <p class="mb-0 small">9 November at 23:29</p>
-                            </div>
-                        </div>
-                        <!-- Card share action menu -->
-                        <a href="#" class="text-secondary btn btn-secondary-soft-hover py-1 px-2" id="cardShareAction5" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-three-dots"></i>
-                        </a>
-                        <!-- Card share action dropdown menu -->
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="cardShareAction5">
-                            <li><a class="dropdown-item" href="#"> <i class="bi bi-bookmark fa-fw pe-2"></i>Save post</a></li>
-                            <li><a class="dropdown-item" href="#"> <i class="bi bi-person-x fa-fw pe-2"></i>Unfollow lori ferguson </a></li>
-                            <li><a class="dropdown-item" href="#"> <i class="bi bi-x-circle fa-fw pe-2"></i>Hide post</a></li>
-                            <li><a class="dropdown-item" href="#"> <i class="bi bi-slash-circle fa-fw pe-2"></i>Block</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#"> <i class="bi bi-flag fa-fw pe-2"></i>Report post</a></li>
-                        </ul>
-                    </div>
-                    <!-- Card share action END -->
-                </div>
-                <!-- Card header START -->
-
-                <!-- Card body START -->
-                <div class="card-body pb-0">
-                    <p>Find out how you can save time in the classroom this year. Earn recognition while you learn new skills on iPad and Mac. Start  recognition your first Apple Teacher badge today!</p>
-                    <!-- Feed react START -->
-                    <ul class="nav nav-stack pb-2 small">
-                        <li class="nav-item">
-                            <a class="nav-link active text-secondary" href="#!"> <i class="bi bi-heart-fill me-1 icon-xs bg-danger text-white rounded-circle"></i> Louis, Billy and 126 others </a>
-                        </li>
-                        <li class="nav-item ms-sm-auto">
-                            <a class="nav-link" href="#!"> <i class="bi bi-chat-fill pe-1"></i>Comments (12)</a>
-                        </li>
-                    </ul>
-                    <!-- Feed react END -->
-                </div>
-                <!-- Card body END -->
-                <!-- Card Footer START -->
-                <div class="card-footer py-3">
-                    <!-- Feed react START -->
-                    <ul class="nav nav-fill nav-stack small">
-                        <li class="nav-item">
-                            <a class="nav-link mb-0 active" href="#!"> <i class="bi bi-heart pe-1"></i>Liked (56)</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mb-0" href="#!"> <i class="bi bi-chat-fill pe-1"></i>Comments (12)</a>
-                        </li>
-                        <!-- Card share action dropdown START -->
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link mb-0" id="cardShareAction6" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="bi bi-reply-fill flip-horizontal ps-1"></i>Share (3)
-                            </a>
-                            <!-- Card share action dropdown menu -->
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="cardShareAction6">
-                                <li><a class="dropdown-item" href="#"> <i class="bi bi-envelope fa-fw pe-2"></i>Send via Direct Message</a></li>
-                                <li><a class="dropdown-item" href="#"> <i class="bi bi-bookmark-check fa-fw pe-2"></i>Bookmark </a></li>
-                                <li><a class="dropdown-item" href="#"> <i class="bi bi-link fa-fw pe-2"></i>Copy link to post</a></li>
-                                <li><a class="dropdown-item" href="#"> <i class="bi bi-share fa-fw pe-2"></i>Share post via …</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#"> <i class="bi bi-pencil-square fa-fw pe-2"></i>Share to News Feed</a></li>
-                            </ul>
-                        </li>
-                        <!-- Card share action dropdown END -->
-                        <li class="nav-item">
-                            <a class="nav-link mb-0" href="#!"> <i class="bi bi-send-fill pe-1"></i>Send</a>
-                        </li>
-                    </ul>
-                    <!-- Feed react END -->
-                </div>
-                <!-- Card Footer END -->
-            </div>
-            <!-- Card feed item END -->
+            <?php endif; ?>
         </div>
         <!-- Main content END -->
 
