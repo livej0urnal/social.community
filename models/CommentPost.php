9e30bfc4f868fddc7c2a9a1e493d698194291502
Igor Filippov <anubis3009@gmail.com>
2023-01-07 16:38:50 +0200

    #about-widget

---
+++ models/CommentPost.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comment_post".
+ *
+ * @property int $id
+ * @property int|null $post_id
+ * @property string|null $created_at
+ * @property string|null $comment
+ * @property int|null $page_id
+ */
+class CommentPost extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'comment_post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['post_id', 'page_id'], 'integer'],
+            [['created_at'], 'safe'],
+            [['comment'], 'string'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'post_id' => 'Post ID',
+            'created_at' => 'Created At',
+            'comment' => 'Comment',
+            'page_id' => 'Page ID',
+        ];
+    }
+}
