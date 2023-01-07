0c175f70e1b25ef83721130ce94413d68df681c6
Igor Filippov <anubis3009@gmail.com>
2023-01-07 21:04:45 +0200

    #delete-comments_with-post

--- components/views/about-profile.php
+++ components/views/about-profile.php
@@ -83,56 +83,6 @@ use yii\helpers\Url;
     </div>
     <!-- Card END -->
 
-    <!-- Card START -->
-    <div class="col-md-6 col-lg-12">
-        <div class="card">
-            <!-- Card header START -->
-            <div class="card-header d-sm-flex justify-content-between border-0">
-                <h5 class="card-title">Photos</h5>
-                <a class="btn btn-primary-soft btn-sm" href="#!"> See all photo</a>
-            </div>
-            <!-- Card header END -->
-            <!-- Card body START -->
-            <div class="card-body position-relative pt-0">
-                <div class="row g-2">
-                    <!-- Photos item -->
-                    <div class="col-6">
-                        <a href="/images/albums/01.jpg" data-gallery="image-popup" data-glightbox="">
-                            <img class="rounded img-fluid" src="/images/albums/01.jpg" alt="">
-                        </a>
-                    </div>
-                    <!-- Photos item -->
-                    <div class="col-6">
-                        <a href="/images/albums/02.jpg" data-gallery="image-popup" data-glightbox="">
-                            <img class="rounded img-fluid" src="/images/albums/02.jpg" alt="">
-                        </a>
-                    </div>
-                    <!-- Photos item -->
-                    <div class="col-4">
-                        <a href="/images/albums/03.jpg" data-gallery="image-popup" data-glightbox="">
-                            <img class="rounded img-fluid" src="/images/albums/03.jpg" alt="">
-                        </a>
-                    </div>
-                    <!-- Photos item -->
-                    <div class="col-4">
-                        <a href="/images/albums/04.jpg" data-gallery="image-popup" data-glightbox="">
-                            <img class="rounded img-fluid" src="/images/albums/04.jpg" alt="">
-                        </a>
-                    </div>
-                    <!-- Photos item -->
-                    <div class="col-4">
-                        <a href="/images/albums/05.jpg" data-gallery="image-popup" data-glightbox="">
-                            <img class="rounded img-fluid" src="/images/albums/05.jpg" alt="">
-                        </a>
-                        <!-- glightbox Albums left bar END  -->
-                    </div>
-                </div>
-            </div>
-            <!-- Card body END -->
-        </div>
-    </div>
-    <!-- Card END -->
-
 <?php if (!empty($friends)) : ?>
     <!-- Card START -->
     <div class="col-md-6 col-lg-12">
