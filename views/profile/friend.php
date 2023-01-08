fa875db1e7291e6d5ba1b3c3e04d0594ca30d8cb
Igor Filippov <anubis3009@gmail.com>
2023-01-08 16:23:01 +0200

    #friend-page

--- views/profile/friend.php
+++ views/profile/friend.php
@@ -28,7 +28,7 @@
                             <!-- Info -->
                             <h1 class="mb-0 h5"><?= $page->page_name ?></h1>
                             <p><?= $page->display_name ?></p>
-                            <p><b><?php echo count($page->feeds); ?> feed(s)</b></p>
+                            <p><b><?php echo count($page->feeds); ?> feed(s)</b> / <b><?php echo count($page->friends)?> friends</b></p>
                         </div>
                         <!-- Button -->
                         <div class="d-flex mt-3 justify-content-center ms-sm-auto">
@@ -61,7 +61,7 @@
                                 <div class="d-flex align-items-center">
                                     <!-- Avatar -->
                                     <div class="avatar me-2">
-                                        <a href="<?= Url::to(['page/profile', 'id' => $post->page_id]) ?>">
+                                        <a href="<?= Url::to(['profile/friend', 'id' => $post->page_id]) ?>">
                                             <?= Html::img($post->page->image, ['alt' => $post->page->display_name, 'class' => 'avatar-img rounded-circle']) ?>
                                         </a>
                                     </div>
@@ -69,7 +69,7 @@
                                     <div>
                                         <div class="nav nav-divider">
                                             <h6 class="nav-item card-title mb-0"><a
-                                                    href="<?= Url::to(['page/profile', 'id' => $post->page_id]) ?>"> <?= $post->page->page_name ?> </a>
+                                                    href="<?= Url::to(['profile/friend', 'id' => $post->page_id]) ?>"> <?= $post->page->page_name ?> </a>
                                             </h6>
                                             <span class="nav-item small"> <?= $post->created_at ?> </span>
                                         </div>
@@ -201,14 +201,72 @@
         <div class="col-lg-4">
 
             <div class="row g-4">
-                <?php
-                //кеш на час
-                if ($this->beginCache('AboutPage', ['duration' => 100])) {
-                    echo \app\components\AboutProfileWidget::widget();
-                    $this->endCache();
-                }
-                ?>
-                <!--                --><? //= \app\components\AboutProfileWidget::widget() ?>
+                <div class="card">
+                    <div class="card-header border-0 pb-0">
+                        <h5 class="card-title">About</h5>
+                        <!-- Button modal -->
+                    </div>
+                    <!-- Card body START -->
+                    <div class="card-body position-relative pt-0">
+                        <p><?= $page->about_page ?></p>
+                    </div>
+                    <!-- Card body END -->
+                </div>
+
+                <div class="card">
+                    <!-- Card header START -->
+                    <div class="card-header d-flex justify-content-between border-0">
+                        <h5 class="card-title">Experience</h5>
+                        <a class="btn btn-primary-soft btn-sm" href="#!"> <i class="fa-solid fa-plus"></i> </a>
+                    </div>
+                    <!-- Card header END -->
+                    <!-- Card body START -->
+                    <div class="card-body position-relative pt-0">
+                        <!-- Experience item START -->
+                        <div class="d-flex">
+                            <!-- Avatar -->
+                            <div class="avatar me-3">
+                                <a href="#!"> <img class="avatar-img rounded-circle" src="/images/logo/08.svg" alt=""> </a>
+                            </div>
+                            <!-- Info -->
+                            <div>
+                                <h6 class="card-title mb-0"><a href="#!"> Apple Computer, Inc. </a></h6>
+                                <p class="small">May 2015 – Present Employment Duration 8 mos <a class="btn btn-primary-soft btn-xs ms-2" href="#!">Edit </a></p>
+                            </div>
+                        </div>
+                        <!-- Experience item END -->
+
+                        <!-- Experience item START -->
+                        <div class="d-flex">
+                            <!-- Avatar -->
+                            <div class="avatar me-3">
+                                <a href="#!"> <img class="avatar-img rounded-circle" src="/images/logo/09.svg" alt=""> </a>
+                            </div>
+                            <!-- Info -->
+                            <div>
+                                <h6 class="card-title mb-0"><a href="#!"> Microsoft Corporation </a></h6>
+                                <p class="small">May 2017 – Present Employment Duration 1 yrs 5 mos <a class="btn btn-primary-soft btn-xs ms-2" href="#!">Edit </a></p>
+                            </div>
+                        </div>
+                        <!-- Experience item END -->
+
+                        <!-- Experience item START -->
+                        <div class="d-flex">
+                            <!-- Avatar -->
+                            <div class="avatar me-3">
+                                <a href="#!"> <img class="avatar-img rounded-circle" src="/images/logo/10.svg" alt=""> </a>
+                            </div>
+                            <!-- Info -->
+                            <div>
+                                <h6 class="card-title mb-0"><a href="#!"> Tata Consultancy Services. </a></h6>
+                                <p class="small mb-0">May 2022 – Present Employment Duration 6 yrs 10 mos <a class="btn btn-primary-soft btn-xs ms-2" href="#!">Edit </a></p>
+                            </div>
+                        </div>
+                        <!-- Experience item END -->
+
+                    </div>
+                    <!-- Card body END -->
+                </div>
 
             </div>
 
