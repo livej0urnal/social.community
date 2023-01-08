6c533d72991d4a73dd7052f0ea6d15515640c074
Igor Filippov <anubis3009@gmail.com>
2023-01-08 17:47:32 +0200

    #friend-page

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -82,7 +82,7 @@ class ProfileController extends AppController
             }
         }
         $friend = Friends::find()->where(['page_id' => $page_user->id])->andWhere(['friend_id' => $page->id])->one();
-        $feed = Feeds::find()->where(['page_id' => $page_user->id])->andWhere(['feed_id' => $page->id])->orWhere(['page_id' => $page->id])->one();
+        $feed = Feeds::find()->where(['page_id' => $page_user->id])->andWhere(['feed_id' => $page->id])->one();
         $this->setMeta($page->page_name);
         return $this->render('friend', compact('page', 'posts', 'pages', 'page_user', 'new_comment', 'friend', 'feed'));
     }
