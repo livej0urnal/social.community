3d6c3b05a5260ac8e0d08d0b4f56f41566b3e0a9
Igor Filippov <anubis3009@gmail.com>
2023-01-15 19:44:34 +0200

    #search-users

--- views/activity/search.php
+++ views/activity/search.php
@@ -1,4 +1,125 @@
 <?php
-    use yii\helpers\Url;
-    use yii\helpers\Html;
+use yii\helpers\Html;
+use yii\helpers\Url;
 ?>
+<div class="container">
+    <div class="row g-4">
+
+        <!-- Sidenav START -->
+        <div class="col-lg-3">
+
+            <!-- Advanced filter responsive toggler START -->
+            <div class="d-flex align-items-center d-lg-none">
+                <button class="border-0 bg-transparent" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSideNavbar" aria-controls="offcanvasSideNavbar">
+                    <i class="btn btn-primary fw-bold fa-solid fa-sliders-h"></i>
+                    <span class="h6 mb-0 fw-bold d-lg-none ms-2">My profile</span>
+                </button>
+            </div>
+            <!-- Advanced filter responsive toggler END -->
+            <?php
+            //кеш на час
+            if ($this->beginCache('SidebarUser', ['duration' => 100])) {
+                echo \app\components\SidebarProfileWidget::widget();
+                $this->endCache(); }
+            ?>
+            <!--            --><?//= \app\components\SidebarProfileWidget::widget() ?>
+        </div>
+        <!-- Sidenav END -->
+
+        <!-- Main content START -->
+        <div class="col-md-8 col-lg-6 vstack gap-4">
+            <!-- Card START -->
+            <div class="card">
+                <!-- Card header START -->
+                <div class="card-header border-0 pb-0">
+                    <div class="row g-2">
+                        <div class="col-lg-12">
+                            <!-- Card title -->
+                            <h1 class="h4 card-title mb-lg-0">Results : <?= $q ?></h1>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card header START -->
+                <!-- Card body START -->
+                <div class="card-body">
+                    <div class="tab-content mb-0 pb-0">
+                        <?php if(!empty($pages)) : ?>
+                        <div class="row g-3">
+                            <div class="col-4">
+                                <!-- Friends item START -->
+                                <div class="card shadow-none text-center h-100">
+                                    <!-- Card body -->
+                                    <div class="card-body p-2 pb-0">
+                                        <div class="avatar avatar-story avatar-xl">
+                                            <a href="/user/111">
+                                                <img class="avatar-img rounded-circle" src="/images/avatar/12.jpg" alt="Quam sed sit temporibus sint.">                                        </a>
+                                        </div>
+                                        <h6 class="card-title mb-1 mt-3"><a href="/user/111"> Miss Emely Walsh DVM </a>
+                                        </h6>
+                                        <p class="mb-0 small lh-sm"></p>
+                                    </div>
+                                    <!-- Card footer -->
+                                    <div class="card-footer p-2 border-0">
+                                        <button class="btn btn-sm btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Send message" aria-label="Send message"><i class="bi bi-chat-left-text"></i></button>
+                                        <button class="btn btn-sm btn-danger delete-friend" data-value="111" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Remove friend"><i class="bi bi-person-x"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <!-- Friends item END -->
+                            </div>
+                            <div class="col-4">
+                                <!-- Friends item START -->
+                                <div class="card shadow-none text-center h-100">
+                                    <!-- Card body -->
+                                    <div class="card-body p-2 pb-0">
+                                        <div class="avatar avatar-story avatar-xl">
+                                            <a href="/user/33">
+                                                <img class="avatar-img rounded-circle" src="/images/avatar/2.jpg" alt="Et dolorum ut ipsum eius.">                                        </a>
+                                        </div>
+                                        <h6 class="card-title mb-1 mt-3"><a href="/user/33"> Else Bailey </a>
+                                        </h6>
+                                        <p class="mb-0 small lh-sm"></p>
+                                    </div>
+                                    <!-- Card footer -->
+                                    <div class="card-footer p-2 border-0">
+                                        <button class="btn btn-sm btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Send message" aria-label="Send message"><i class="bi bi-chat-left-text"></i></button>
+                                        <button class="btn btn-sm btn-danger delete-friend" data-value="33" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Remove friend"><i class="bi bi-person-x"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <!-- Friends item END -->
+                            </div>
+                            <div class="col-4">
+                                <!-- Friends item START -->
+                                <div class="card shadow-none text-center h-100">
+                                    <!-- Card body -->
+                                    <div class="card-body p-2 pb-0">
+                                        <div class="avatar avatar-story avatar-xl">
+                                            <a href="/user/111">
+                                                <img class="avatar-img rounded-circle" src="/images/avatar/12.jpg" alt="Quam sed sit temporibus sint.">                                        </a>
+                                        </div>
+                                        <h6 class="card-title mb-1 mt-3"><a href="/user/111"> Miss Emely Walsh DVM </a>
+                                        </h6>
+                                        <p class="mb-0 small lh-sm"></p>
+                                    </div>
+                                    <!-- Card footer -->
+                                    <div class="card-footer p-2 border-0">
+                                        <button class="btn btn-sm btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Send message" aria-label="Send message"><i class="bi bi-chat-left-text"></i></button>
+                                        <button class="btn btn-sm btn-danger delete-friend" data-value="111" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Remove friend"><i class="bi bi-person-x"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <!-- Friends item END -->
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <!-- Card body END -->
+            </div>
+            <!-- Card END -->
+        </div>
+        <!-- Right sidebar END -->
+
+    </div> <!-- Row END -->
+</div>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -70,8 +70,8 @@ Header START -->
                 <!-- Nav Search START -->
                 <div class="nav mt-3 mt-lg-0 flex-nowrap align-items-center px-4 px-lg-0">
                     <div class="nav-item w-100">
-                        <form class="rounded position-relative">
-                            <input class="form-control ps-5 bg-light" type="search" placeholder="Search..." aria-label="Search">
+                        <form class="rounded position-relative" method="get" action="<?= \yii\helpers\Url::to(['activity/search']) ?>">
+                            <input name="q" <?php if(strlen($_REQUEST['q']) > 1) : ?> value="<?php echo $_REQUEST['q'] ;?>" <?php endif; ?> class="form-control ps-5 bg-light" type="search" placeholder="Search..." aria-label="Search" required>
                             <button class="btn bg-transparent px-2 py-0 position-absolute top-50 start-0 translate-middle-y" type="submit"><i class="bi bi-search fs-5"> </i></button>
                         </form>
                     </div>
