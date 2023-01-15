ff69ed755dc04082faf8fc2f0309057951eb1c68
Igor Filippov <anubis3009@gmail.com>
2023-01-15 17:05:23 +0200

    #factory-content_create

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,13 +4,38 @@ namespace app\controllers;
 
 use app\controllers\AppController;
 use app\models\Category;
+use app\models\News;
 use Faker\Factory;
 use Yii;
+use yii\filters\AccessControl;
 
 class NewsController extends AppController
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['*'], // действия в контроллере
+                'rules' => [ // правила к действиям
+                    [
+                        'allow' => true,
+                        'actions' => ['*'], // действия в контроллере
+                        'roles' => ['?'], // Доступ к действиям только для не авторизованных пользователей
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['index', 'single', 'fake'], // действия в контроллере
+                        'roles' => ['@'], // Доступ к действиям только для авторизованных пользователей
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
+        $id = Yii::$app->request->get('id');
         $this->setMeta('Latest News ');
         return $this->render('index');
     }
@@ -24,14 +49,22 @@ class NewsController extends AppController
     public function actionFake()
     {
         $faker = Factory::create();
-        for($i = 0; $i < 30; $i++){
-            $category = new Category();
-            $category->title = $faker->word();
-            $category->save();
-            //            $friends = new Friends();
-//            $friends->page_id = 1;
-//            $friends->friend_id = rand(1, 300);
-//            $friends->save(false);
+        for($i = 0; $i < 100; $i++){
+//            $category = new Category();
+//            $category->title = $faker->word();
+//            $category->save();
+
+//            $post = new News();
+//            $post->slug = $faker->slug();
+//            $post->title = $faker->jobTitle();
+//            $post->content = $faker->text(400);
+//            $post->category_id = rand(1,30);
+//            $post->page_id = rand(1, 10);
+//            $post->image = '/images/post/4by3/0'. rand(1,6) .'.jpg';
+//            $new_date = $faker->dateTimeThisYear();
+//            $post->public_date = $new_date->format('Y-m-d H:i:s');
+//            $post->save();
+
         }
 
         die('Data generation is complete!');
