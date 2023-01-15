ef7e45d391974da379520b3c0e26188b7a585c90
Igor Filippov <anubis3009@gmail.com>
2023-01-15 17:13:39 +0200

    #sidebar_blog-widget

--- components/SidebarBlogWidget.php
+++ components/SidebarBlogWidget.php
@@ -2,6 +2,7 @@
 
 namespace app\components;
 
+use app\models\Pages;
 use yii\base\Widget;
 use app\models\Category;
 use app\models\News;
@@ -11,7 +12,9 @@ class SidebarBlogWidget extends Widget
 {
     public function run()
     {
+        $user = Yii::$app->user->identity->id;
+        $page = Pages::find()->where(['user_id' => $user])->with('feeds')->one();
         $categories = Category::find()->orderBy(['title' => SORT_DESC])->all();
-        return $this->render('sidebar-blog', compact('categories'));
+        return $this->render('sidebar-blog', compact('categories', 'page'));
     }
 }
