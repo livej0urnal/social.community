a677ec00d3ab87aa674cdb3c06984b2e11a93d83
Igor Filippov <anubis3009@gmail.com>
2023-01-15 19:12:06 +0200

    #start-notifications

--- controllers/ActivityController.php
+++ controllers/ActivityController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\controllers\AppController;
+use app\models\Pages;
 use Yii;
 
 class ActivityController extends AppController
@@ -10,6 +11,8 @@ class ActivityController extends AppController
     public function actionIndex()
     {
         $user = Yii::$app->user->identity->id;
-        return $this->render('index');
+        $page = Pages::findOne(['user_id' => $user]);
+        $this->setMeta('Notifications '. $page->page_name);
+        return $this->render('index', compact('page'));
     }
 }
