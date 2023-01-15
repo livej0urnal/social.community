f634a33d4d4c3caf53c9913ffb37a2393c4c503e
Igor Filippov <anubis3009@gmail.com>
2023-01-15 17:20:36 +0200

    #feed-widget-sidebar

--- components/SidebarBlogWidget.php
+++ components/SidebarBlogWidget.php
@@ -14,7 +14,8 @@ class SidebarBlogWidget extends Widget
     {
         $user = Yii::$app->user->identity->id;
         $page = Pages::find()->where(['user_id' => $user])->with('feeds')->one();
+        $posts = News::find()->where(['!=', 'page_id', $page->id])->orderBy(['rand()' => SORT_DESC])->limit(5)->all();
         $categories = Category::find()->orderBy(['title' => SORT_DESC])->all();
-        return $this->render('sidebar-blog', compact('categories', 'page'));
+        return $this->render('sidebar-blog', compact('categories', 'page', 'posts'));
     }
 }
